7551b41a55ed4c23a78aa5b8f2c5af7f4511e73d
datnt <ntdat89.hn@gmail.com>
2022-05-01 22:59:34 +0700

    .

--- src/constant.js
+++ src/constant.js
@@ -19,7 +19,7 @@ export const DARK_THEME = {
   TEXT_COLOR_SECONDARY: '#e1e1e1',
   DISABLED_COLOR: '#d2d2d2',
   BORDER_COLOR_BASE: '#737373',
-  BOX_SHADOW_BASE: '0 3px 6px -4px rgb(255 255 255 / 12%), 0 6px 16px 0 rgb(255 255 255 / 8%), 0 9px 28px 8px rgb(255 255 255 / 5%)',
+  BOX_SHADOW_BASE: '0 3px 6px -4px rgba(255, 255, 255, 0.12), 0 6px 16px rgba(255, 255, 255, 0.08), 0 9px 28px 8px rgba(255, 255, 255, 0.05)',
 }
 export const LIGHT_THEME = {
   PRIMARY_COLOR: '#0465B0',
@@ -33,8 +33,8 @@ export const LIGHT_THEME = {
   TEXT_COLOR: 'rgba(0, 0, 0, 0.65)',
   TEXT_COLOR_SECONDARY: 'rgba(0, 0, 0, 0.45)',
   DISABLED_COLOR: 'rgba(0, 0, 0, 0.12)',
-  BORDER_COLOR_BASE: '#dedede',
-  BOX_SHADOW_BASE: '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08),0 9px 28px 8px rgba(0, 0, 0, 0.05)',
+  BORDER_COLOR_BASE: '#d9d9d9',
+  BOX_SHADOW_BASE: '0 3px 6px -4px #0000001f, 0 6px 16px #00000014, 0 9px 28px 8px #0000000d',
 }
 // endregion
 // region Properties server
